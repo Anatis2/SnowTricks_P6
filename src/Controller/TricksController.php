60c4d95b7dabe87cd033b3b0a1df2b9cbdfa9b98
Anatis2 <claire.coubard@gmail.com>
2020-01-24 12:25:36 +0100

    Formulaire de création de figures : 1ère passe

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use App\Form\TrickType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,5 +16,29 @@ class TricksController extends AbstractController
     {
         return $this->render('tricks/home.html.twig');
     }
+
+	/**
+	 * @Route("/creer", name="createTrick")
+	 */
+	public function createTrick()
+	{
+
+		$trick = new Trick();
+
+		$form = $this->createFormBuilder($trick)
+					 ->add('name')
+					 ->add('description')
+					 ->getForm();
+
+		return $this->render('tricks/createTrick.html.twig', [
+			'form' => $form->createView()
+		]);
+	}
+
+
+
+
+
+
 
 }
