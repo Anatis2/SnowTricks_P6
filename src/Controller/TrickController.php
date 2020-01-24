66ab16cab8f56c6b61a142ff3dfbe862e79a5707
Anatis2 <claire.coubard@gmail.com>
2020-01-24 17:28:23 +0100

    1. Consulter la liste des figures : Afficher la liste des figures présentes dans la BDD en page d’accueil

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -4,19 +4,24 @@ namespace App\Controller;
 
 use App\Entity\Trick;
 use App\Form\TrickType;
+use App\Repository\TrickRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TricksController extends AbstractController
+class TrickController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function home()
+    public function home(TrickRepository $repo)
     {
-        return $this->render('tricks/home.html.twig');
+    	$tricks = $repo->findAll();
+
+        return $this->render('tricks/home.html.twig', [
+			'tricks' => $tricks
+		]);
     }
 
 	/**
